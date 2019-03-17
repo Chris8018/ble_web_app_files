4168b83566bf6844a2c9e22f0c325582ebb1805c
Chris8018 <tranuytrieuvi@gmail.com>
2019-03-17 20:01:49 +0000

    Test 2 services on Android

--- ti_sensor_tag.js
+++ ti_sensor_tag.js
@@ -104,7 +104,7 @@ class TISensorTag {
 
     getServices(server, services, characteristics) {
         // May be something is wrong here
-        // self.getModelName(server, services[0], characteristics[0]);
+        self.getModelName(server, services[0], characteristics[0]);
         self.getIRTemperature(server, services[1], characteristics.slice(1));
         // return server.getPrimaryService(services[0].uuid)
         // .then(service => {
