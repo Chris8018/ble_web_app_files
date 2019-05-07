88102a332fed2dba388eb9a62660f6b48016c6ec
Chris8018 <tranuytrieuvi@gmail.com>
2019-05-07 07:28:52 +0100

    Try download api

--- index.js
+++ index.js
@@ -109,4 +109,34 @@ window.onload = function () {
     function smoothingData() {
         // implelment
     }
+
+    let downloadButton= document.getElementById('download');
+
+    downloadButton.onclick = e => {
+        // implement
+        console.log("download butoon is clicked");
+        download("hello.txt","This is the content of my file :)");
+
+    }
+
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+    
+        element.style.display = 'none';
+        document.body.appendChild(element);
+    
+        element.click();
+    
+        document.body.removeChild(element);
+    }
 }
+
+// document.getElementById("dwn-btn").addEventListener("click", function(){
+//     // Generate download of hello.txt file with some content
+//     var text = document.getElementById("text-val").value;
+//     var filename = "hello.txt";
+    
+//     download(filename, text);
+// }, false);
