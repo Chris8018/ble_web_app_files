a659d562ba4a00290861fe75a5e30f48dc0d27c1
Chris8018 <tranuytrieuvi@gmail.com>
2019-05-07 06:01:37 +0100

    Connect to 2 tags

--- index.js
+++ index.js
@@ -7,27 +7,33 @@
  * Waiting for site to load before running
  */
 window.onload = function () {
-    let conButton = document.getElementById('connect');
-    let disButton = document.getElementById('disconnect');
-
     // Check if browser support Web bluetooth API
     if ('bluetooth' in navigator === false) {
         alert('Browser does not support the Web Bluetooth API');
     }
 
-    let ti_sensortag;
-    let gyroData;
-    let accData;
-    let magData;
+    let conB1 = document.getElementById('connect1');
+    let disB1 = document.getElementById('disconnect1');
+    
+    let ti_sensortag1;
+    let gyroDataReal1 = [];
+    let gyroDataSmoothed1 = [];
 
-    conButton.onclick = e => {
-        ti_sensortag = new MotionSensor();
-        ti_sensortag.connect();
+    let accDataReal1 = [];
+    let accDataSmothed1 = [];
 
-        ti_sensortag.onStateChange(state => {
-            gyroData = state.gyroData;
-            accData = state.accData;
-            magData = state.magData;
+    // let magData;
+
+    conB1.onclick = e => {
+        ti_sensortag1 = new MotionSensor();
+        ti_sensortag1.connect();
+
+        ti_sensortag1.onStateChange(state => {
+            gyroDataReal1.push(state.gyroData);
+            accDataReal1.push(state.accData);
+            // gyroData = state.gyroData;
+            // accData = state.accData;
+            // magData = state.magData;
 
             displayData();
         })
@@ -35,26 +41,44 @@ window.onload = function () {
 
     function displayData() {
 
-        if (gyroData) {
-            document.getElementById('gyroX').innerHTML = gyroData.x;
-            document.getElementById('gyroY').innerHTML = gyroData.y;
-            document.getElementById('gyroZ').innerHTML = gyroData.z;
+        if (gyroDataReal1.length > 0) {
+            let tempGyro = gyroDataReal1[gyroDataReal1.length - 1];
+            // Data smoothing here
+            document.getElementById('gyroX').innerHTML = tempGyro.x;
+            document.getElementById('gyroY').innerHTML = tempGyro.y;
+            document.getElementById('gyroZ').innerHTML = tempGyro.z;
         }
 
-        if (accData) {
-            document.getElementById('accX').innerHTML = accData.x;
-            document.getElementById('accY').innerHTML = accData.y;
-            document.getElementById('accZ').innerHTML = accData.z;
+        if (accDataReal1.length > 0) {
+            let tempAcc = accDataReal1[accDataReal1.length - 1];
+            // Data smoothing here
+            document.getElementById('accX').innerHTML = tempAcc.x;
+            document.getElementById('accY').innerHTML = tempAcc.y;
+            document.getElementById('accZ').innerHTML = tempAcc.z;
         }
 
-        if (magData) {
-            document.getElementById('magX').innerHTML = magData.x;
-            document.getElementById('magY').innerHTML = magData.y;
-            document.getElementById('magZ').innerHTML = magData.z;
-        }
+        // if (magData) {
+        //     document.getElementById('magX').innerHTML = magData.x;
+        //     document.getElementById('magY').innerHTML = magData.y;
+        //     document.getElementById('magZ').innerHTML = magData.z;
+        // }
     }
 
-    disButton.onclick = e => {
+    disB1.onclick = e => {
         ti_sensortag.disconnect();
+    }
+
+    let conB2 = document.getElementById('connect2');
+    let disB2 = document.getElementById('disconnect2');
+
+    let ti_sensortag2;
+    let gyroDataReal2 = [];
+    let gyroDataSmoothed2 = [];
+
+    let accDataReal2 = [];
+    let accDataSmoothed2 = [];
+
+    function smoothingData() {
+        // implelment
     }
 }
